1019d1695c24ca1cecabfce0b11e82156b1b91db
Pedro Roccon <pedro@pedroroccon.com.br>
2021-01-25 11:40:47 +0000

    Adicionando o favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="api-url" content="{{ url('/') }}" />
+        <meta name="api-url" content="{{ url('/') }}"/>
 
         <title>{{ config('app.name') }}</title>
+        <link rel="icon" type="image/svg+xml" href="{{ asset('svg/favicon.svg') }}">
+
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
         <script src="{{ asset('js/app.js') }}" defer></script>
